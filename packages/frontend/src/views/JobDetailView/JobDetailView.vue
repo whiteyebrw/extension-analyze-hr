363c687b967d5862bf6593e943d298bd3843fd66
whiteyebrw <syst1337@gmail.com>
2025-03-12 21:02:34 +0300

    feat: added resume matched

--- packages/frontend/src/views/JobDetailView/JobDetailView.vue
+++ packages/frontend/src/views/JobDetailView/JobDetailView.vue
@@ -1,30 +1,34 @@
 <script setup lang="ts">
 import { useApi } from '@/app/api';
 import { useRoute } from 'vue-router';
-import VueMarkdown from 'vue-markdown-render';
 
 const route = useRoute();
 
-const { data } = await useApi(`/jobs/${route.params.id}`).json();
+const { data, } = await useApi(`/jobs/${route.params.id}`, {
+	afterFetch(ctx) {
+		ctx.data.matchedResumes = ctx.data.matchedResumes.reduce((acc, item) => {
+			acc[item.id] = item.match_percentage;
+			return acc;
+		}, {});
+
+		return ctx;
+	},
+}).json();
 </script>
 
 <template>
 	<Card>
+		<template #title>Вакансия</template>
 		<template #content>
 			<p class="break-all">{{ data.vacancyUrl }}</p>
 		</template>
 	</Card>
-	<Card v-for="resumeUrl in data.resumeUrls" :key="resumeUrl">
+	<Card v-for="(resumeUrl, idx) in data.resumeUrls" :key="resumeUrl">
+		<template #title>Кандидат {{ idx + 1 }}</template>
 		<template #content>
 			<p class="break-all">{{ resumeUrl }}</p>
-		</template>
-	</Card>
-	<Card>
-		<template #title>Результат</template>
-		<template #content>
-			<ScrollPanel style="height: 450px">
-				<vue-markdown :source="data.result "/>
-			</ScrollPanel>
+			<p class="break-all">Процент совпадения:
+				{{ data.matchedResumes[resumeUrl] !== undefined ? data.matchedResumes[resumeUrl] : '-' }} </p>
 		</template>
 	</Card>
 </template>
