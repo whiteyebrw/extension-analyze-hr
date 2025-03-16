b6557678198fe05b3eb7a2198f97fd41741b0338
whiteyebrw <syst1337@gmail.com>
2025-03-16 21:07:51 +0300

    feat: added extraction skills from description

--- packages/backend-analyze/main.py
+++ packages/backend-analyze/main.py
@@ -1,11 +1,28 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from pydantic import BaseModel
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 from sklearn.metrics.pairwise import cosine_similarity
 from typing import List, Dict
+import spacy
 
-app = FastAPI()
+nlp = None
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global nlp
+    nlp = spacy.load("en_core_web_lg")
+    skill_pattern_path = "jz_skill_patterns.jsonl"
+
+    ruler = nlp.add_pipe("entity_ruler")
+    ruler.from_disk(skill_pattern_path)
+    yield
+    nlp.remove_pipe("entity_ruler")
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 class Candidate(BaseModel):
@@ -21,7 +38,8 @@ class Vacancy(BaseModel):
 
 def get_matched_candidates(vacancy_skills: List[str], candidates: List[Candidate]):
     vectorizer = TfidfVectorizer()
-    all_texts = [" ".join(vacancy_skills)] + [" ".join(c.skills) for c in candidates]
+    all_texts = [" ".join(vacancy_skills)] + [" ".join(unique_skills(get_skills(c.description) + c.skills)) for c in
+                                              candidates]
     vectors = vectorizer.fit_transform(all_texts)
 
     num_clusters = 2
@@ -43,9 +61,25 @@ def get_matched_candidates(vacancy_skills: List[str], candidates: List[Candidate
     return matched_candidates
 
 
+def get_skills(text):
+    doc = nlp(text)
+    myset = []
+    subset = []
+    for ent in doc.ents:
+        if ent.label_ == "SKILL" or ent.label_ == "SOFT-SKILL":
+            subset.append(ent.text)
+    myset.append(subset)
+    return subset
+
+
+def unique_skills(x):
+    return list(set(x))
+
+
 @app.post("/match_candidates/")
 def match_candidates(vacancy: Vacancy, candidates: List[Candidate]) -> Dict:
-    matched_candidates = get_matched_candidates(vacancy.skills, candidates)
+    vacancy_skills = unique_skills(get_skills(vacancy.description) + vacancy.skills)
+    matched_candidates = get_matched_candidates(vacancy_skills, candidates)
 
     result = {
         "matched_candidates": [
